f519326be23937fa7ad9309a703050a0b8ef18b2
Michael Rodriguez <miarodriguezur@unal.edu.co>
2022-09-10 15:39:14 -0500

    Se agrega proyecto maven-spring para manejo del backend

---
+++ proyecto-techome/src/main/java/co/edu/upt/misiontic/g22_2022/proyectotechome/ProyectoTechomeApplication.java
@@ -0,0 +1,13 @@
+package co.edu.upt.misiontic.g22_2022.proyectotechome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoTechomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoTechomeApplication.class, args);
+	}
+
+}
---
+++ proyecto-techome/src/test/java/co/edu/upt/misiontic/g22_2022/proyectotechome/ProyectoTechomeApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.upt.misiontic.g22_2022.proyectotechome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoTechomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
