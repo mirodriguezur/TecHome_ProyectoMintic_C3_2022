5d19685bba8bc755dfe008f40a456703aa427421
Michael Rodriguez <miarodriguezur@unal.edu.co>
2022-09-24 23:59:08 -0500

    Se agrega Oportunidades de negocio, y se cambia footer

---
+++ proyecto-techome/src/main/resources/static/js/OportunidadesNegocio.js
@@ -0,0 +1,3 @@
+function informacion() {
+    alert("En esta página  encontrará información sobre los proyectos que hay cerca a su área. \n Haga buen uso de la información  de los consumidores pues son sus potenciales clientes.")
+}
